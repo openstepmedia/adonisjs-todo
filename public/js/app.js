b4c2b9939c5baa25555759baae7ec2538ef6b64a
sethm <seth@openstepmedia.com>
2018-02-07 20:09:47 -0700

    cleanup client

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
-    //let socket = io(app.config.app_url);
     console.log("app_url:" + app.config.app_url)
 
-    var socket = io(app.config.app_url);
+    var socket = io.connect(app.config.app_url, {
+        //ref: https://stackoverflow.com/questions/11089545/stop-socket-io-from-reconnecting
+        reconnection: false
+    })
+
     socket.on('current_time', function(data) {
         console.log('current_time %o', data)
     })
 
+    socket.on('disconnect', function(){
+        console.log('disconnected')
+    })
 })
