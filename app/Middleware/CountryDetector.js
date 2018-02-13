fa31103723552bfb24eed144a8313e3439c074ee
sethm <seth@openstepmedia.com>
2018-02-13 14:05:27 -0700

    Add CountryDetector middleware

--- app/Middleware/CountryDetector.js
+++ app/Middleware/CountryDetector.js
@@ -2,10 +2,27 @@
 
 const geoip = use('geoip-lite')
 
+/*
+|--------------------------------------------------------------------------
+| Global Middleware
+|--------------------------------------------------------------------------
+|
+| ref: http://adonisjs.com/docs/4.1/middleware#_creating_middleware
+|
+*/
 class CountryDetector {
   async handle ({ request }, next) {
     const ip = request.ip()
-    request.country = geoip.lookup(ip).country
+    
+    try {
+      request.country = geoip.lookup(ip).country
+    } catch (err) {
+      /**
+       * local vm throws an error.
+       * @type type
+       */
+      console.error("Could not determine country for ip:" + ip + " error:", err)
+    }
     
     // call next to advance the request
     await next()
